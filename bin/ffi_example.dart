ff3fc764a0be04f2bfd7361cf09866bcf28bd9c1
Daniel Alvarez <daniel.alvarez@tii.ae>
2023-10-17 21:40:10 +0100

    :sparkles: Split into FFI and FFIGen cases

--- bin/ffi_example.dart
+++ bin/ffi_example.dart
@@ -5,11 +5,32 @@ import 'package:ffi_example/generated_bindings.dart';
 final path = "native_code/build/libmultiplyLib.so";
 final DynamicLibrary _dylib = DynamicLibrary.open(path);
 
-final NativeLibrary _bindings = NativeLibrary(_dylib);
 
 void main() {
+  multiplyUsingFFIGen();
+  multiplyUsingFFI();
+}
+
+void multiplyUsingFFIGen() {
+  // This is the generated bindings from the FFIGen tool.
+  final NativeLibrary _bindings = NativeLibrary(_dylib);
+
   var firstDigit = 10;
   var secondDigit = 2;
   final result = _bindings.multiply(firstDigit, secondDigit);
-  print("$firstDigit * $secondDigit = $result");
+  print("Using FFIGen -> $firstDigit * $secondDigit = $result");
 }
+
+void multiplyUsingFFI() {
+  // If you don't want to use the FFIGen tool, you can use the FFI directly.
+  final int Function(int, int) multiplyFFI = _dylib
+      .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("multiply")
+      .asFunction();
+
+  var firstDigit = 10;
+  var secondDigit = 2;
+  final result = multiplyFFI(firstDigit, secondDigit);
+  print("Using FFI -> $firstDigit * $secondDigit = $result");
+}
+
+
