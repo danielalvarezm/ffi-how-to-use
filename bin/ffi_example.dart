e70a0b2fdaca83e26ed2b25592469182418147db
Daniel Alvarez <daniel.alvarez@tii.ae>
2023-10-27 15:58:40 +0100

    :recycle: Extract code to classes

--- bin/ffi_example.dart
+++ bin/ffi_example.dart
@@ -3,35 +3,49 @@ import 'package:ffi_example/generated_bindings.dart';
 
 // This path is relative to the root of the project, and may need to be changed depending on where you run the example from.
 final path = "native_code/build/libmathOperationsLib.so";
-final DynamicLibrary _dylib = DynamicLibrary.open(path);
+final DynamicLibrary _dynamicLib = DynamicLibrary.open(path);
 
 void main() {
   var firstDigit = 10;
   var secondDigit = 2;
 
-  mathOperationsWithFFIGen(firstDigit, secondDigit);
-  mathOperationsWithFFI(firstDigit, secondDigit);
+  // with ffigen
+  final mathOperationsFFIGen = MathOperationsFFIGen(_dynamicLib);
+  print("Multiply with ffigen: ${mathOperationsFFIGen.multiply(firstDigit, secondDigit)}");
+  print("Add with ffigen: ${mathOperationsFFIGen.add(firstDigit, secondDigit)}");
+
+  // with ffi
+  final mathOperationsWithFFI = MathOperationsWithFFI(_dynamicLib);
+  print("Multiply with ffi: ${mathOperationsWithFFI.multiply(firstDigit, secondDigit)}");
+  print("Add with ffi: ${mathOperationsWithFFI.add(firstDigit, secondDigit)}");
 }
 
-void mathOperationsWithFFIGen(int firstDigit, int secondDigit) {
-  // This is the generated bindings from the FFIGen tool.
-  final NativeLibrary _bindings = NativeLibrary(_dylib);
+class MathOperationsFFIGen {
+  final NativeLibrary _bindings;
 
-  final multiplyResult = _bindings.multiply(firstDigit, secondDigit);
-  final addResult = _bindings.add(firstDigit, secondDigit);
+  MathOperationsFFIGen(DynamicLibrary dynamicLibrary) : _bindings = NativeLibrary(dynamicLibrary); // Generated bindings from the FFIGen tool.
 
-  print("Using FFIGen -> $firstDigit * $secondDigit = $multiplyResult");
-  print("Using FFIGen -> $firstDigit + $secondDigit = $addResult");
+  int multiply(int firstDigit, int secondDigit) {
+    return _bindings.multiply(firstDigit, secondDigit);
+  }
+
+  int add(int firstDigit, int secondDigit) {
+    return _bindings.add(firstDigit, secondDigit);
+  }
 }
 
-void mathOperationsWithFFI(int firstDigit, int secondDigit) {
-  // If you don't want to use the FFIGen tool, you can use the FFI directly.
-  final int Function(int, int) multiplyFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("multiply").asFunction();
-  final int Function(int, int) addFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("add").asFunction();
+class MathOperationsWithFFI {
+  final DynamicLibrary _dylib;
 
-  final multiplyResult = multiplyFFI(firstDigit, secondDigit);
-  final addResult = addFFI(firstDigit, secondDigit);
+  MathOperationsWithFFI(this._dylib);
 
-  print("Using FFI -> $firstDigit * $secondDigit = $multiplyResult");
-  print("Using FFI -> $firstDigit + $secondDigit = $addResult");
+  int multiply(int firstDigit, int secondDigit) {
+    final int Function(int, int) multiplyFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("multiply").asFunction();
+    return multiplyFFI(firstDigit, secondDigit);
+  }
+
+  int add(int firstDigit, int secondDigit) {
+    final int Function(int, int) addFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("add").asFunction();
+    return addFFI(firstDigit, secondDigit);
+  }
 }
