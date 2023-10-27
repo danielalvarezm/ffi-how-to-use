94b74812ab6caa343a38787101b485649338e8f5
Daniel Alvarez <daniel.alvarez@tii.ae>
2023-10-27 15:43:22 +0100

    :recycle: Add parameters to methods

--- bin/ffi_example.dart
+++ bin/ffi_example.dart
@@ -6,36 +6,32 @@ final path = "native_code/build/libmathOperationsLib.so";
 final DynamicLibrary _dylib = DynamicLibrary.open(path);
 
 void main() {
-  mathOperationsWithFFIGen();
-  mathOperationsWithFFI();
+  var firstDigit = 10;
+  var secondDigit = 2;
+
+  mathOperationsWithFFIGen(firstDigit, secondDigit);
+  mathOperationsWithFFI(firstDigit, secondDigit);
 }
 
-void mathOperationsWithFFIGen() {
+void mathOperationsWithFFIGen(int firstDigit, int secondDigit) {
   // This is the generated bindings from the FFIGen tool.
   final NativeLibrary _bindings = NativeLibrary(_dylib);
 
-  var firstDigit = 10;
-  var secondDigit = 2;
   final multiplyResult = _bindings.multiply(firstDigit, secondDigit);
-  print("Using FFIGen -> $firstDigit * $secondDigit = $multiplyResult");
   final addResult = _bindings.add(firstDigit, secondDigit);
+
+  print("Using FFIGen -> $firstDigit * $secondDigit = $multiplyResult");
   print("Using FFIGen -> $firstDigit + $secondDigit = $addResult");
 }
 
-void mathOperationsWithFFI() {
+void mathOperationsWithFFI(int firstDigit, int secondDigit) {
   // If you don't want to use the FFIGen tool, you can use the FFI directly.
-  final int Function(int, int) multiplyFFI = _dylib
-      .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("multiply")
-      .asFunction();
+  final int Function(int, int) multiplyFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("multiply").asFunction();
+  final int Function(int, int) addFFI = _dylib.lookup<NativeFunction<Int32 Function(Int32, Int32)>>("add").asFunction();
 
-  final int Function(int, int) addFFI = _dylib
-      .lookup<NativeFunction<Int32 Function(Int32, Int32)>>("add")
-      .asFunction();
-
-  var firstDigit = 10;
-  var secondDigit = 2;
   final multiplyResult = multiplyFFI(firstDigit, secondDigit);
-  print("Using FFI -> $firstDigit * $secondDigit = $multiplyResult");
   final addResult = addFFI(firstDigit, secondDigit);
+
+  print("Using FFI -> $firstDigit * $secondDigit = $multiplyResult");
   print("Using FFI -> $firstDigit + $secondDigit = $addResult");
 }
